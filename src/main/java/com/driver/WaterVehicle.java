e02983a43d8dd821482632ca6e0c16ecb64207e3
91894@PRANSHU <pranshubarar1851996@gmail.com>
2023-01-21 15:24:12 +0530

    Solution 2

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -5,24 +5,4 @@ public interface WaterVehicle {
     public int getVehicleCapacity();
 }
 
-class Boat implements WaterVehicle {
 
-    private String name;
-    private int capacity;
-
-
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String getVehicleName() {
-        return this.name;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return this.capacity;
-    }
-}
